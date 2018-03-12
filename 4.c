35dd539ea37e84607b54b2922028be0b38486187
kavinandhu1001 <30829042+kavinandhu1001@users.noreply.github.com>
2018-03-12 16:09:02 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+    char str[10],s=".";
+    gets(str);
+    int i=0;
+   for(i=0;str[i]!='\0';i++)
+  
+    
+    {
+        if(str[i]=='\0')
+        {
+            printf("%c",s);
+        }
+        else
+        {
+                printf("%c",str[i]);
+        
+        }
+        
+    
+}  
+
+}
